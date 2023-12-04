4c16850c6d9733e8528c61c32cb08200f8be5696
Giancarlo Salomone <g.salomone@live.com>
2023-12-04 20:09:51 -0300

    Desafio Concluido

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-asadas
+class Heroi {    
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;                
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'usou um ataque indefinido';
+        }
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+}
+
+const heroiMago = new Heroi('Merlin', 100, 'mago');
+const heroiGuerreiro = new Heroi('Arthur', 30, 'guerreiro');
+const heroiMonge = new Heroi('Alice', 40, 'monge');
+const heroiNinja = new Heroi('Kunoichi', 25, 'ninja');
+
+heroiMago.atacar();
+heroiGuerreiro.atacar();
+heroiMonge.atacar();
+heroiNinja.atacar();
